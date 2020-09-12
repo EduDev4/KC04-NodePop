5214cc7de21b26e9b30a0ee4ff69f21957eacccf
Eduardo Fernandez <edufernandez4@gmail.com>
2020-09-12 13:16:30 +0200

    feat: corregida lectura de parametros para que funcione paginación

--- nodepop/routes/api/advertisements.js
+++ nodepop/routes/api/advertisements.js
@@ -2,22 +2,47 @@ var express = require('express');
 var router = express.Router();
 const Advertisement = require('../../models/Advertisement');
 
-/* GET /Advertisement */
+/* GET Advertisements quering fields */
 router.get('/', async function(req, res, next) {
   try {
     const filter = {};
+
+    //Extract values from Request and add them to Filter
+    const name = req.query.name;
+    for(var key in req.query){     
+      if(key!='limit' && key!='skip' && key!='sort' && key!='fields'){
+        filter[key] = req.query[key]
+      }
+    }    
+    // http://localhost:3000/api/ads?limit=2
+    const limit = parseInt(req.query.limit || 10);
+    // http://localhost:3000/api/ads?skip=20&limit=10
+    const skip = parseInt(req.query.skip || 0);
+    const sort = 'name';
+     
+    const fields = req.query.fields;
     
-    
+    const advertisements = await Advertisement.list(filter, limit, skip, sort, fields);
+    res.json(advertisements);
+  } catch (err) {
+    next(err);
+  }
+
+});
+
+router.get('/price', async function(req, res, next) {
+  try {
+    const filter = {};
 
     //Extract values from Request
     const name = req.query.name;
     for(var key in req.query){
       console.log(key + " : " + req.query[key]);
-    }
-    filter.name=name;
-    // http://localhost:3000/api/agentes?limit=2
+      filter[key] = req.query[key]
+    }    
+    // http://localhost:3000/api/ads?limit=2
     const limit = parseInt(req.query.limit || 10);
-    // http://localhost:3000/api/agentes?skip=20&limit=10
+    // http://localhost:3000/api/ads?skip=20&limit=10
     const skip = parseInt(req.query.skip || 0);
     const sort = 'name';
      
